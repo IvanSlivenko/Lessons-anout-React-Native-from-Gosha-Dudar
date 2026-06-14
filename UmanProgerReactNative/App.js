a0c35d65f8d4e2833733f86706b2334e1f0eb429
Ivan Slivenko <umanproger@ukr.net>
2026-06-14 04:33:46 +0300

    14-06-2026-1

--- UmanProgerReactNative/App.js
+++ UmanProgerReactNative/App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, Alert } from 'react-native';
+
 
 export default function App() {
+  const handleTextPress = () => console.log('Text pressed')
+  const handleButtonPress = () => {
+    console.log('handleButtonPress_Click')
+    Alert.alert("UmanProger", "Main message", [{ text: "Так" }, { text: "Відмінити" }])
+  };
   return (
-    <View style={styles.container}>
-      <Text>Hello My APP !!!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text
+        numberOfLines={2}
+        style={styles.text}
+        onPress={handleTextPress}
+      >
+        Hello {'\n'}My APP !!!
+      </Text>
+      <Button
+        title="Натисни на мене"
+        color='yellow'
+        onPress={handleButtonPress}
+      />
+
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView >
   );
 }
 
@@ -14,7 +33,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  text: {
+    color: 'yellow'
+  },
+
+
 });
