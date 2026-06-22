3c024af9419b04c43534dda5a36071321a59e25e
Ivan Slivenko <umanproger@ukr.net>
2026-06-22 08:06:47 +0300

    22-06-2026-1

--- UmanProgerReactNative_SDK_54/components/Main.js
+++ UmanProgerReactNative_SDK_54/components/Main.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Button, TouchableOpacity, FlatList, Image } from 'react-native';
+import { StyleSheet, View, Text, Button, TouchableOpacity, FlatList, Image, Modal } from 'react-native';
 import { useFonts } from 'expo-font';
 import { gStyle } from '../styles/style';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function Main({ navigation }) {
 
@@ -12,15 +14,29 @@ export default function Main({ navigation }) {
         { name: 'name 3', anons: 'anons 3', full: 'full text 3, full text 3', key: '3', img: 'https://i.gordonua.com/news/2026/6/6/51376289028461438a0076k.jpg?size=960x540' }
     ])
 
+    const [modalWindow, setmodalWindow] = useState(false);
+
+
+
+
     return (
         <View style={gStyle.main}>
-            {/* <Text style={[gStyle.title, styles.header]}>Новини</Text> */}
+            <Modal visible={modalWindow}>
+                <View style={gStyle.main}>
+                    <MaterialCommunityIcons name="close-circle-outline" size={34} color="#8e0cf8" style={styles.iconClose} onPress={() => setmodalWindow(false)} />
+                    <Text style={styles.title}>Форма додавання статей</Text>
+                </View>
+
+            </Modal>
+
+            <Ionicons name="add-circle-outline" size={34} color="#6d3602" style={styles.iconAdd} onPress={() => setmodalWindow(true)} />
             <Text style={[gStyle.title, styles.header]}>Новини</Text>
             <FlatList data={news} renderItem={({ item }) => (
                 <TouchableOpacity style={styles.item} onPress={() => navigation.navigate('FullInfo', item)}>
                     <Image style={styles.image} source={{ uri: item.img }} />
                     <Text style={styles.title}>{item.name}</Text>
                     <Text style={styles.anons}>{item.anons}</Text>
+
 
                 </TouchableOpacity>
             )} />
@@ -55,5 +71,15 @@ const styles = StyleSheet.create({
     image: {
         weight: '100%',
         height: 200
+    },
+    iconAdd: {
+        textAlign: 'center',
+        marginBottom: 15
+    },
+    iconClose: {
+        textAlign: 'right',
+        marginTop: 5,
+        marginRight: 5
+
     }
 });
